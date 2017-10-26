60c9e35813c99d7afdd7ce85301554b6d716ed03
root <root@cld-foreman.cloud.pd.infn.it>
2017-10-26 16:16:49 +0200

    Leave default verbosity for glance-registry

--- controller_ocata/manifests/configure_glance.pp
+++ controller_ocata/manifests/configure_glance.pp
@@ -74,7 +74,7 @@ define remove_config ($conf_file, $section, $param, $value) {
   # glance-registry.conf
 
   do_config { 'glance_reg_db': conf_file => '/etc/glance/glance-registry.conf', section => 'database', param => 'connection', value => $controller_ocata::params::glance_db, }
-  do_config { 'glance_reg_image_verbose': conf_file => '/etc/glance/glance-registry.conf', section => 'DEFAULT', param => 'verbose', value => false, }
+#  do_config { 'glance_reg_image_verbose': conf_file => '/etc/glance/glance-registry.conf', section => 'DEFAULT', param => 'verbose', value => false, }
   do_config { 'glance_reg_image_size_cap': conf_file => '/etc/glance/glance-registry.conf', section => 'DEFAULT', param => 'image_size_cap', value => $controller_ocata::params::glance_image_size_cap, }
   do_config { 'glance_reg_auth_uri': conf_file => '/etc/glance/glance-registry.conf', section => 'keystone_authtoken', param => 'auth_uri', value => $controller_ocata::params::auth_uri, }
   do_config { 'glance_reg_auth_url': conf_file => '/etc/glance/glance-registry.conf', section => 'keystone_authtoken', param => 'auth_url', value => $controller_ocata::params::auth_url, }
