749796539b9f46822f0303a5def610769616203f
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2017-12-06 11:46:32 +0100

    Fixed aai db migration

--- controller_ocata/manifests/configure_horizon.pp
+++ controller_ocata/manifests/configure_horizon.pp
@@ -64,18 +64,14 @@ class controller_ocata::configure_horizon inherits controller_ocata::params {
 
     exec { "create-$aai_db_name-db":
         command => "/usr/bin/mysql -u root -h ${aai_db_host} -e \"create database IF NOT EXISTS ${aai_db_name}; grant all on ${aai_db_name}.* to ${aai_db_user}@localhost identified by '${aai_db_pwd}'; grant all on ${aai_db_name}.* to ${aai_db_user}@'${vip_mgmt}' identified by '${aai_db_pwd}'; grant all on ${aai_db_name}.* to ${aai_db_user}@'${ip_ctrl1}' identified by '${aai_db_pwd}'; grant all on ${aai_db_name}.* to ${aai_db_user}@'${ip_ctrl2}' identified by '${aai_db_pwd}';\"",
-        unless => "/usr/bin/mysql -u root -h ${aai_db_host} -e \"show DATABASES LIKE '${aai_db_name}';\"",
+        unless  => "/usr/bin/mysql -u root -h ${aai_db_host} -e \"show DATABASES LIKE '${aai_db_name}';\"",
+        require => Package["mariadb"],
     }
 
     exec { "makemigrations_db":
-        command => "/usr/bin/python /usr/share/openstack-dashboard/manage.py makemigrations --name madebypuppet openstack_auth_shib",
-        creates => "/usr/lib/python2.7/site-packages/openstack_auth_shib/migrations/",
-    }
-
-    exec { "migrate_db":
-        command => "/usr/sbin/runuser -s /bin/bash -c 'python /usr/share/openstack-dashboard/manage.py migrate' -- apache",
-        onlyif => "/usr/bin/test ! -e /usr/lib/python2.7/site-packages/openstack_auth_shib/migrations/*_madebypuppet.py",
-        #onlyif => "/usr/bin/test ! -e /usr/lib/python2.7/site-packages/openstack_auth_shib/migrations/*_initialstep.py",
+        command => "/usr/bin/python /usr/share/openstack-dashboard/manage.py makemigrations --name ocata_changes openstack_auth_shib && /usr/sbin/runuser -s /bin/bash -c 'python /usr/share/openstack-dashboard/manage.py migrate' -- apache",
+        onlyif  => "/usr/bin/test ! -e /usr/lib/python2.7/site-packages/openstack_auth_shib/migrations/*_ocata_changes.py",
+        require => Exec["create-$aai_db_name-db"],
     }
 
   ### Patch for AAI testing IdP
